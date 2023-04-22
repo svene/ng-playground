d0ddc7e722e8b845f45c39c02aa43de3b119b867
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-04-22 10:47:17 +0200

    added two nested components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {Level1Component} from "./level1/level1.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    Level1Component
+  ],
+  standalone: true
 })
 export class AppComponent {
   title = 'ng15';
