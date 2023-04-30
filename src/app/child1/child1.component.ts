6545cd99ad25b06626baef2aef741cbc89c4f6e1
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-04-30 09:53:31 +0200

    cleanup

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -6,7 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './child1.component.html',
-  styleUrls: ['./child1.component.scss']
 })
 export class Child1Component {
 
