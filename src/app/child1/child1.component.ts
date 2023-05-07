0709f5188ba3d1669259d4a6041201cd4db17016
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-05-07 18:08:24 +0200

    http call

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {BaloiseDesignSystemModule} from "@baloise/design-system-components-angular";
-import {balIconCheck, balIconRefresh} from "@baloise/design-system-icons";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {BehaviorSubject, filter, Observable, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-child1',
   standalone: true,
-  imports: [CommonModule, BaloiseDesignSystemModule],
+  imports: [CommonModule, BaloiseDesignSystemModule, HttpClientModule],
   templateUrl: './child1.component.html',
   styleUrls: ['./child1.component.scss']
 })
-export class Child1Component {
+export class Child1Component implements OnInit {
 
-  protected readonly balIconRefresh = balIconRefresh;
-  protected readonly balIconCheck = balIconCheck;
+  private http = inject(HttpClient);
+  apiResponse$!: Observable<unknown>;
+
+  requestApiCall$$ = new BehaviorSubject(0);
+  requestApiCall$ = this.requestApiCall$$.asObservable();
+
+  callAPI(): void {
+    this.requestApiCall$$.next(1);
+  }
+
+  ngOnInit(): void {
+    this.apiResponse$ = this.requestApiCall$.pipe(
+      filter(it => it > 0),
+      switchMap(_ => this.http.get<string>('https://thronesapi.com/api/v2/Characters')),
+    );
+  }
 }
